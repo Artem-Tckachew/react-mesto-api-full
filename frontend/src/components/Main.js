4e02a0c9d88a6ca049bfc784558b5157f2bf6a6d
Artem-Tckachew <79772674001@ya.ru>
2021-08-31 11:54:34 +0300

    Работа в разработке

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -3,22 +3,21 @@ import Card from './Card';
 import { CurrentUserContext } from '../contexts/CurrentUserContext'
 import Preloader from "./Preloader";
 
-function Main({cards, onEditProfile, onAddPlace, onEditAvatar, onCardClick, onCardLike, onCardDelete, isCardsLoading,
-  isCardsError}) {
+function Main({props}) {
   const currentUser = React.useContext(CurrentUserContext);
   
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar" name="avatar"  style={{ backgroundImage: `url(${currentUser.avatar})` }} >
-          <div className="profile__avatar profile__avatar_hover" onClick={onEditAvatar} />
+          <div className="profile__avatar profile__avatar_hover" onClick={props.onEditAvatar} />
         </div>
         <div className="profile__intro">
           <h2 className="profile__title" name="name">{currentUser.name}</h2>
           <p className="profile__subtitle" name="about">{currentUser.about}</p>
-          <button type="button" className="profile__edit" onClick={onEditProfile} />
+          <button type="button" className="profile__edit" onClick={props.onEditProfile} />
         </div>
-        <button type="button" className="profile__add" onClick={onAddPlace} />
+        <button type="button" className="profile__add" onClick={props.onAddPlace} />
       </section>
       <section className="elements">
       {isCardsLoading && (
