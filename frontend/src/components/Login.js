cef67792d02096dc55dd76d3ac8b548d569b3a08
Artem-Tckachew <79772674001@ya.ru>
2021-08-31 13:53:29 +0300

    Работа в разработке

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Login (props){
+function Login ({ onLogin }){
   const [password, setPassword] = React.useState('');
   const [email, setEmail] = React.useState('');
 
@@ -14,17 +14,14 @@ function Login (props){
 
   function handleSubmit(evt){
     evt.preventDefault();
-    props.onSubmit({
-      email,
-      password
-    })
+    onLogin({ email, password });
   }
 
   return (
     <div className="auth">
       <form className="auth__form" onSubmit={handleSubmit}>
         <div className="auth__wrapper">
-          <h3 className="auth__title">{props.title}</h3>
+          <h3 className="auth__title">Вход</h3>
           <label className="auth__input">
             <input type="email" name="email" id="email" className="auth__textfield" placeholder="Email" onChange={handleEmailChange} required  />
           </label>
@@ -32,7 +29,7 @@ function Login (props){
             <input type="password" name="password" id="password" className="auth__textfield" placeholder="Пароль" onChange={handlePasswordChange} required  />
           </label>
         </div>
-        <button className="auth__button" type="submit">{props.button}</button>
+        <button className="auth__button" type="submit">Войти</button>
       </form>
     </div>
   )
