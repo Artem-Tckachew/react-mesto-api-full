40ebb702fe96ee65ed0fa431ef89fbe0c52335b1
Artem-Tckachew <79772674001@ya.ru>
2021-09-01 08:24:12 +0300

    Работа в разработке

--- frontend/src/components/EditAvatarPopup.js
+++ frontend/src/components/EditAvatarPopup.js
@@ -1,25 +1,23 @@
-import { PopupWithForm } from './PopupWithForm.js';
-import React from 'react';
+import React, {useEffect} from 'react';
+import PopupWithForm from './PopupWithForm'
+import {UseFormValidation} from './UseFormValidation'
 
-export function EditAvatarPopup(props) {
-
-  const avaLinkInput = React.useRef()
+function EditAvatarPopup({isOpen, onUpdateAvatar, onClose, isSending }) {
+  const {values, handleChange, resetFrom, errors, isValid} = UseFormValidation();
+ 
+  useEffect(() => {
+    resetFrom({});
+  }, [isOpen, resetFrom]);
 
   function handleSubmit(e) {
     e.preventDefault();
-
-    props.onUpdateAvatar({
-      avatar: avaLinkInput.current.value,
-    }
-    )
-    avaLinkInput.current.value = "";
-
+    onUpdateAvatar(values);
   }
 
   return (
 
-    <PopupWithForm onSubmit={handleSubmit} onClose={props.onClose} isOpen={props.isOpen} name="avatar" title="Обновить аватар" text={isSending ? "Сохранение..." : "Сохранить" } isDisabled={!isValid}>
-    <input ref={avaLinkInput}  className="form__input form__input_avatar popup__input" id="avatar" name="avatar" placeholder="Ссылка на аватар" required />
+    <PopupWithForm isOpen={isOpen} onSubmit={handleSubmit} onClose={onClose} name="avatar" title="Обновить аватар" text={isSending ? "Сохранение..." : "Сохранить" } isDisabled={!isValid}>
+    <input type="url" value={values.avatar || ''} onChange={handleChange} className="form__input form__input_avatar popup__input" id="avatar" name="avatar" placeholder="Ссылка на аватар" required />
     <span className="popup__error_visible" id='avatar-error'>{errors.avatar || ""}</span>
 </PopupWithForm>
   )}
