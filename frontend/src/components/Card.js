b1751371385d98d235869e98fce6a11000ce5833
Artem-Tckachew <79772674001@ya.ru>
2021-09-01 10:52:47 +0300

    Работа в разработке

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -15,13 +15,13 @@ const Card = ({ card, onCardLike, onCardDelete, onCardClick }) => {
   function handleLikeClick() {
     onCardLike(card)
   }
-  const isOwn = card.owner._id === currentUser._id;
+  const isOwn = card.owner === currentUser._id;
 
   const cardDeleteButtonClassName = (
     `element__remove ${isOwn ? 'element__remove_active' : 'element__remove_innactive'}`
   );
 
-  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const isLiked = card.likes.some(i => i === currentUser._id);
   
   const cardLikeButtonClassName = `element__like ${isLiked ? 'element__like_active' : ''}`;
   return(
