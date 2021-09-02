5a3d4ae143a6d5bf5512768a86500c2b857b8f82
Artem Tkachev <79772674001@ya.ru>
2021-09-02 18:53:39 +0300

    Работа в разработке

--- backend/app.js
+++ backend/app.js
@@ -7,25 +7,27 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { Joi, celebrate } = require('celebrate');
 const rateLimit = require('express-rate-limit');
-const { isURL } = require('validator');
 const { login, createUser, logout } = require('./controllers/users');
 const auth = require('./middlewares/auth');
-const NotFoundError = require('./errors/NotFoundError');
-const serverError = require('./middlewares/serverError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const app = express();
 const { PORT = 3000 } = process.env;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
 app.use(helmet());
+app.disable('x-powered-by');
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.use(requestLogger);
-app.use(cors({
-  origin: true,
-  credentials: true,
-}));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -34,46 +36,40 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().email(),
     password: Joi.string().required().min(8),
   }),
 }), login);
-
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom((value) => {
-      if (!isURL(value)) {
-        throw new Error('Ссылка некоректная');
-      }
-      return value;
+    avatar: Joi.string().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      } return helpers.message('Переданна не валидная ссылка');
     }),
-  }).unknown(true),
+  }),
 }), createUser);
 app.delete('/signout', logout);
+
 app.use(auth);
+
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
-
-app.use('*', () => {
-  throw new NotFoundError('Запрашиваемый ресурс не найден.');
-});
+app.use('*', require('./routes/notFound'));
 
 app.use(errorLogger);
 app.use(errors());
-
-app.use(serverError);
+app.use(errorHandler);
 
 app.listen(PORT);
